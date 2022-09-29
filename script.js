77532fc3a69c520d7036cfa0ae6db4f23e07232f
Utkir <karshi2700@gmail.com>
2022-09-29 16:43:29 +0500

    JavaScript Preority of operations

---
+++ script.js
@@ -0,0 +1,10 @@
+var result = 12 - 6 / 3;
+var resul2 = 3 + 4 * 2;
+
+var isGreater = 20 - 6 * 3 >= 4;
+var isGreater2 = 20 - 6 * 3 >= 1;
+
+console.log("Result1:", result);
+console.log("Result2:", resul2);
+console.log("Result3:", isGreater);
+console.log("Result4:", isGreater2);
